6f6947c80f367774e40349649b27f08eaf95c112
Bruk Argaw <bkargaw@gmail.com>
2017-06-27 18:27:47 -0700

    working on delete metod on bst

--- lib/bst.rb
+++ lib/bst.rb
@@ -1,5 +1,5 @@
 class BSTNode
-  attr_accessor :left, :right
+  attr_accessor :left, :right, :parent
   attr_reader :value
 
   def initialize(value)
@@ -7,7 +7,7 @@ class BSTNode
   end
 
 end
-
+require 'byebug'
 class BinarySearchTree
   def initialize
     @root = nil
@@ -52,9 +52,13 @@ class BinarySearchTree
   def self.insert!(node, value)
     return BSTNode.new(value) if node == nil
     if node.value >= value
-      node.left = BinarySearchTree.insert!(node.left, value)
+      child = BinarySearchTree.insert!(node.left, value)
+      child.parent = node
+      node.left = child
     elsif node.value < value
-      node.right = BinarySearchTree.insert!(node.right, value)
+      child = BinarySearchTree.insert!(node.right, value)
+      child.parent = node
+      node.right = child
     end
 
     node
@@ -68,15 +72,24 @@ class BinarySearchTree
   end
 
   def self.preorder!(node)
-
+    return if node.nil?
+    left = BinarySearchTree.inorder!(node.left)
+    right = BinarySearchTree.inorder!(node.right)
+    [node.value] + left  + right
   end
 
   def self.inorder!(node)
-
+    return [] if node.nil?
+    left = BinarySearchTree.inorder!(node.left)
+    right = BinarySearchTree.inorder!(node.right)
+    left + [node.value] + right
   end
 
   def self.postorder!(node)
-
+    return [] if node.nil?
+    left = BinarySearchTree.inorder!(node.left)
+    right = BinarySearchTree.inorder!(node.right)
+    left + right + [node.value]
   end
 
   def self.height!(node)
@@ -103,10 +116,35 @@ class BinarySearchTree
   end
 
   def self.delete_min!(node)
-
+    return if node.nil?
+    min_node = BinarySearchTree.min(node)
+    return if  node == min_node
+    # debugger
+    min_node.parent.left = min_node.right
+    min_node.right.parent = min_node.parent
+    min_node
   end
 
   def self.delete!(node, value)
+    return if node.nil?
+    d_node = BinarySearchTree.find!(node, value)
 
+    return if node.nil?
+    parent = d_node.parent
+    if d_node.left && d_node.right
+
+    elsif d_node.left || d_node.right
+      child = d_node.left
+      if parent.left.value == d_node.value
+        BinarySearchTree.reassing(parent, d_node.right, 'left')
+      parent.right = nil if parent.right.value == d_node.value
+    else
+      return nil
+    end
+  end
+
+  def self.reassing(parent, new_child, side)
+    new_child.parent = parent
+    side == 'left' ? parent.left = new_child : parent.right = new_child
   end
 end
